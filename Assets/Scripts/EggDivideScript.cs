935168a4092bf408c7cd44f9c2a7e8db9d4e2c8e
sirpodge <pdmongey@gmail.com>
2014-12-19 16:43:33 +0000

    Updated Touch Controls

--- Assets/Scripts/EggDivideScript.cs
+++ Assets/Scripts/EggDivideScript.cs
@@ -14,13 +14,7 @@ public class EggDivideScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		lineGO = new GameObject("Line");
-		lineGO.AddComponent<LineRenderer>();
-		lineRenderer = lineGO.GetComponent<LineRenderer>();
-		lineRenderer.material = new Material(Shader.Find("Mobile/Particles/Additive"));
-		lineRenderer.SetColors(c1, c2);
-		lineRenderer.SetWidth(0.3F, 0);
-		lineRenderer.SetVertexCount(0);
+
 		anim = gameObject.GetComponent<Animator> ();
 	}
 
@@ -32,47 +26,9 @@ public class EggDivideScript : MonoBehaviour {
 		}
 	void OnCollisionEnter(Collision other)
 	{
-				if (other.gameObject.name == "Line") {
-					anim.SetTrigger ("Stage1");
-				}
-		}
-			// Update is called once per frame
-	void Update () {
-
-		if (Input.touchCount > 0)
-		{
-			Touch touch = Input.GetTouch(0);
-			
-			if(touch.phase == TouchPhase.Moved)
-			{
-				lineRenderer.SetVertexCount(i+1);
-				Vector3 mPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15);
-				lineRenderer.SetPosition(i, Camera.main.ScreenToWorldPoint(mPosition));
-				i++;
-				
-				/* Add Collider */
-				
-				BoxCollider bc = lineGO.AddComponent<BoxCollider>();
-				bc.transform.position = lineRenderer.transform.position;
-				bc.size = new Vector3(0.1f, 0.1f, 0.1f);
+			if (other.gameObject.name == "Line") {
+				anim.SetTrigger ("Stage1");
 			}
-			
-			if(touch.phase == TouchPhase.Ended)
-			{
-				/* Remove Line */
-				
-				lineRenderer.SetVertexCount(0);
-				i = 0;
-				
-				/* Remove Line Colliders */
-				
-				BoxCollider[] lineColliders = lineGO.GetComponents<BoxCollider>();
-				
-				foreach(BoxCollider b in lineColliders)
-				{
-					Destroy(b);
-				}
-			}
-		}
 	}
+
 }
