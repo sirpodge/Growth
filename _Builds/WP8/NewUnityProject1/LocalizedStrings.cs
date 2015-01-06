c8ca9364e7d73d77c0fd6984201fed913518e835
JohnHealyPulse <john@pulsecollege.eu>
2015-01-06 15:31:24 +0000

    WP8 Build Added

---
+++ _Builds/WP8/NewUnityProject1/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+using NewUnityProject1.Resources;
+
+namespace NewUnityProject1
+{
+	/// <summary>
+	/// Provides access to string resources.
+	/// </summary>
+	public class LocalizedStrings
+	{
+		private static AppResources _localizedResources = new AppResources();
+
+		public AppResources LocalizedResources { get { return _localizedResources; } }
+	}
+}
