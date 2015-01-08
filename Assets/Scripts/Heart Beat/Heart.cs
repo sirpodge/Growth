ec6ed1046cf204a3e38d79a77813dbd083353612
xIIPANIKIIx <dylanconvery@yahoo.com>
2015-01-08 22:32:00 +0000

    f

--- Assets/Scripts/Heart Beat/Heart.cs
+++ Assets/Scripts/Heart Beat/Heart.cs
@@ -3,27 +3,55 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class Heart : MonoBehaviour {
-
-	public float timeTaken = 0;
+	
+	public float timeTaken =0;
+	public float taps;
+	public bool checker1;
 	public GameObject can;
 	Text textco;
-
-	void Update()
+	
+	void Start()
 	{
 		textco = can.GetComponentInChildren<Text>();
+	}
+	
+	void Update()
+	{
 		textco.text = timeTaken.ToString();
 		if (Input.touchCount > 0)
 		{
 			Touch touch = Input.touches[0];
-			switch (touch.phase)
+			if(touch.phase == TouchPhase.Began)
 			{
-				case TouchPhase.Began:
-				timeTaken = Time.deltaTime;
-				break;
-					
-				case TouchPhase.Ended:
-				break;
+				taps++;
 			}
 		}
+		if(taps >0)
+		{
+			if(taps <3)
+			{
+				if(timeTaken < 0.5)
+				{
+					if(taps ==2 && timeTaken <0.4 && timeTaken > 0.2)
+					{
+						Debug.Log("You win!");//Dumb code everywhere!
+					}
+					timeTaken += Time.deltaTime;
+				}
+
+				else
+				{
+					taps = 0;
+					timeTaken = 0;
+				}
+			}
+			else
+			{
+				taps = 0;
+				timeTaken = 0;
+			}
+		}
+
 	}
 }
+	
---
+++ Assets/Scripts/Heart Beat/fingerIcon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class fingerIcon : MonoBehaviour {
+	private Vector2 startPos;
+	public float alphaChannel;
+	bool touched;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		alphaChannel = Mathf.Clamp01(alphaChannel);
+		alphaChannel += -1f * 1.5f * Time.deltaTime;
+		gameObject.GetComponent<Image>().color = new Color(gameObject.GetComponent<Image>().color.r,gameObject.GetComponent<Image>().color.g,gameObject.GetComponent<Image>().color.b,alphaChannel);
+
+		if (Input.touchCount > 0) //If there's a finger on the screen
+		{
+			Touch touch = Input.touches[0];
+			if(touch.phase == TouchPhase.Began)
+			{
+				alphaChannel = 0.5f;
+				startPos = touch.position;
+				transform.position = new Vector2(startPos.x, startPos.y);
+			}
+		}
+	}
+}
