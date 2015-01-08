4734ed7cc12823ff6c5234412e570af27f43944c
xIIPANIKIIx <dylanconvery@yahoo.com>
2015-01-08 14:34:08 +0000

    from yesterday

--- Assets/Scripts/Heart Beat/fingerIcon.cs
+++ Assets/Scripts/Heart Beat/fingerIcon.cs
@@ -10,22 +10,31 @@ public class fingerIcon : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	public void setTrailRenderer()
+	{
+		TrailRenderer tr = this.GetComponent<TrailRenderer>();
+		tr.sortingLayerName = "Foreground";
+	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		alphaChannel = Mathf.Clamp01(alphaChannel);
 		alphaChannel += -1f * 1.5f * Time.deltaTime;
-		gameObject.GetComponent<Image>().color = new Color(gameObject.GetComponent<Image>().color.r,gameObject.GetComponent<Image>().color.g,gameObject.GetComponent<Image>().color.b,alphaChannel);
+		//gameObject.GetComponent<Image>().color = new Color(gameObject.GetComponent<Image>().color.r,gameObject.GetComponent<Image>().color.g,gameObject.GetComponent<Image>().color.b,alphaChannel);
 
 		if (Input.touchCount > 0) //If there's a finger on the screen
 		{
 			Touch touch = Input.touches[0];
-			if(touch.phase == TouchPhase.Began)
+			if(touch.phase != TouchPhase.Ended)
 			{
 				alphaChannel = 0.5f;
 				startPos = touch.position;
-				transform.position = new Vector2(startPos.x, startPos.y);
+				Debug.Log(touch.position);
+				//transform.position = new Vector3(startPos.x, startPos.y, 0f);
+			
+
 			}
 		}
 	}
--- Assets/Scripts/SwipeGame/Swipe.cs
+++ Assets/Scripts/SwipeGame/Swipe.cs
@@ -4,31 +4,48 @@ using System.Collections;
 
 public class Swipe : MonoBehaviour 
 {
-	private Vector2 startPos;
+	private Vector2 startPos = Vector2.zero;
 	public Sprite stage_2, stage_3;
+	Touch touch;
 	bool stage1;
+	LineRenderer swipeLine;
+
+	void Start()
+	{
+		swipeLine = gameObject.GetComponent<LineRenderer> ();
+	}
+
 
 	void Update()
 	{
+
+
 		if (Input.touchCount > 0) //If there's a finger on the screen
 		{
-			Touch touch = Input.touches[0];//Only count 1
+			touch = Input.touches[0];//Only count 1
+
+
+
 			switch (touch.phase)//Touch function switch
 			{
 				case TouchPhase.Began://First touch, stores where it begins
 				startPos = touch.position;
+
 				break;//jumps to next phase
-		
+
 				case TouchPhase.Ended://When I take my finger up
+				Debug.DrawLine(startPos, touch.position, Color.white,1f);
+				Debug.Log(startPos);
+				Debug.Log(touch.position);
 				if(!stage1)
 				{
-					if(startPos.x > 100f && startPos.x < 200f)
+					if(startPos.x > 80f && startPos.x < 140f)
 					{
-						if(touch.position.x > 100f && touch.position.x < 200f)
+						if(touch.position.x > 80f && touch.position.x < 140f)
 						{
-							if(startPos.y < 400f && startPos.y > 280f)
+							if(startPos.y < 345f && startPos.y > 260f)
 							{
-								if(touch.position.y < 190f && touch.position.y > 100f)
+								if(touch.position.y < 145f && touch.position.y > 25f)
 								{
 									gameObject.GetComponent<Image>().sprite = stage_2;
 									stage1 = true;
@@ -36,7 +53,10 @@ public class Swipe : MonoBehaviour
 							}
 						}
 					}
+
 				}
+
+
 				else
 				{
 					if(startPos.y < 320f && startPos.y > 180f)
@@ -50,6 +70,7 @@ public class Swipe : MonoBehaviour
 									gameObject.GetComponent<Image>().sprite = stage_3;
 									GameObject.Find("Background").GetComponent<LickTheRainbow>().enabled = true;
 									gameObject.GetComponent<LickTheRainbow>().enabled = true;
+									Debug.DrawLine(startPos,touch.position, Color.white);
 								}
 							}
 						}
