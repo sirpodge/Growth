2aaff99b7b42972bbb80d9f91b57d0f9459dd104
xIIPANIKIIx <dylanconvery@yahoo.com>
2015-01-08 17:42:38 +0000

    new

--- Assets/Scripts/Heart Beat/fingerIcon.cs
+++ Assets/Scripts/Heart Beat/fingerIcon.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class fingerIcon : MonoBehaviour {
-	private Vector2 startPos;
+	private Vector3 startPos;
 	public float alphaChannel;
 	bool touched;
 	// Use this for initialization
@@ -22,17 +22,17 @@ public class fingerIcon : MonoBehaviour {
 	{
 		alphaChannel = Mathf.Clamp01(alphaChannel);
 		alphaChannel += -1f * 1.5f * Time.deltaTime;
-		//gameObject.GetComponent<Image>().color = new Color(gameObject.GetComponent<Image>().color.r,gameObject.GetComponent<Image>().color.g,gameObject.GetComponent<Image>().color.b,alphaChannel);
+		gameObject.GetComponent<Image>().color = new Color(gameObject.GetComponent<Image>().color.r,gameObject.GetComponent<Image>().color.g,gameObject.GetComponent<Image>().color.b,alphaChannel);
 
 		if (Input.touchCount > 0) //If there's a finger on the screen
 		{
 			Touch touch = Input.touches[0];
-			if(touch.phase != TouchPhase.Ended)
+			if(touch.phase == TouchPhase.Began)
 			{
 				alphaChannel = 0.5f;
 				startPos = touch.position;
 				Debug.Log(touch.position);
-				//transform.position = new Vector3(startPos.x, startPos.y, 0f);
+				transform.position = new Vector3(startPos.x, startPos.y, startPos.z);
 			
 
 			}
--- Assets/Scripts/SwipeGame/Swipe.cs
+++ Assets/Scripts/SwipeGame/Swipe.cs
@@ -5,79 +5,122 @@ using System.Collections;
 public class Swipe : MonoBehaviour 
 {
 	private Vector2 startPos = Vector2.zero;
-	public Sprite stage_2, stage_3;
+	public Image egg_1, egg_2, advance, finger;
 	Touch touch;
-	bool stage1;
-	LineRenderer swipeLine;
+	public bool stage1;
+	public bool stage2;
+	bool begin;
+	float alphaChannel;
+	float timer = 2.5f;
+
+
+	public Animator anim;
 
 	void Start()
 	{
-		swipeLine = gameObject.GetComponent<LineRenderer> ();
+
+		if(gameObject.name == "Egg_3")
+		{
+			anim = GetComponent<Animator>();
+		}
+
 	}
+
+
 
 
 	void Update()
 	{
+		if (!begin) {
+						timer -= Time.deltaTime;
+						if (timer <= 0) {
+								begin = true;
+				if(finger)
+				{
+					finger.gameObject.SetActive(false);
+				}
+
+						}
+				}
+
+		if (begin) {
+				if (stage1) {
+						if (gameObject.name == "Egg_1") {
+								if (!stage2) {
+										egg_1.color = new Color (egg_1.color.r, egg_1.color.g, egg_1.color.b, Mathf.Clamp01 (egg_1.color.a) + 1f * 0.5f * Time.deltaTime);
+										GetComponent<Image> ().color = new Color (GetComponent<Image> ().color.r, GetComponent<Image> ().color.g, GetComponent<Image> ().color.b, Mathf.Clamp01 (GetComponent<Image> ().color.a) - 1f * 0.5f * Time.deltaTime);
+								}
+						}
 
 
-		if (Input.touchCount > 0) //If there's a finger on the screen
-		{
-			touch = Input.touches[0];//Only count 1
 
+				}
 
-
-			switch (touch.phase)//Touch function switch
+			if (stage2) 
 			{
-				case TouchPhase.Began://First touch, stores where it begins
-				startPos = touch.position;
-
-				break;//jumps to next phase
-
-				case TouchPhase.Ended://When I take my finger up
-				Debug.DrawLine(startPos, touch.position, Color.white,1f);
-				Debug.Log(startPos);
-				Debug.Log(touch.position);
-				if(!stage1)
+				if (gameObject.name == "Egg_2") 
 				{
-					if(startPos.x > 80f && startPos.x < 140f)
-					{
-						if(touch.position.x > 80f && touch.position.x < 140f)
-						{
-							if(startPos.y < 345f && startPos.y > 260f)
-							{
-								if(touch.position.y < 145f && touch.position.y > 25f)
-								{
-									gameObject.GetComponent<Image>().sprite = stage_2;
-									stage1 = true;
-								}
-							}
-						}
-					}
+					Debug.Log ("hh");
 
+					GetComponent<Image> ().color = new Color (GetComponent<Image> ().color.r, GetComponent<Image> ().color.g, GetComponent<Image> ().color.b, Mathf.Clamp01 (GetComponent<Image> ().color.a) - 1f * 0.5f * Time.deltaTime);
+					egg_2.color = new Color (egg_2.color.r, egg_2.color.g, egg_2.color.b, Mathf.Clamp01 (egg_2.color.a) + 1f * 0.5f * Time.deltaTime);
+					advance.color = new Color (advance.color.r, advance.color.g, advance.color.b, Mathf.Clamp01 (advance.color.a) + 1f * 0.5f * Time.deltaTime);
 				}
 
-
-				else
+				if(gameObject.name == "Egg_3")
 				{
-					if(startPos.y < 320f && startPos.y > 180f)
+					if(Mathf.Clamp01(GameObject.Find("Egg_2").GetComponent<Image>().color.a) == 0)
 					{
-						if(touch.position.y < 320f && touch.position.y > 180f)
-						{
-							if(startPos.x > 0f && startPos.x < 100f)
-							{
-								if(touch.position.x > 200f && touch.position.x < 310f)
-								{
-									gameObject.GetComponent<Image>().sprite = stage_3;
-									GameObject.Find("Background").GetComponent<LickTheRainbow>().enabled = true;
-									gameObject.GetComponent<LickTheRainbow>().enabled = true;
-									Debug.DrawLine(startPos,touch.position, Color.white);
-								}
-							}
-						}
+						Debug.Log("hi");
+						anim.SetBool("go", true);
 					}
 				}
-				break;
 			}
+
+				if (Input.touchCount > 0) { //If there's a finger on the screen
+						touch = Input.touches [0];//Only count 1
+
+
+
+						switch (touch.phase) {//Touch function switch
+						case TouchPhase.Began://First touch, stores where it begins
+								startPos = touch.position;
+
+								break;//jumps to next phase
+
+						case TouchPhase.Ended://When I take my finger up
+								Debug.DrawLine (startPos, touch.position, Color.white, 3f);
+								Debug.Log (startPos);
+								Debug.Log (touch.position);
+								if (!stage1) {
+										if (startPos.x > 130f && startPos.x < 220f) {
+												if (touch.position.x > 30f && touch.position.x < 150f) {
+														if (startPos.y < 340f && startPos.y > 260f) {
+																if (touch.position.y < 160f && touch.position.y > 65f) {
+
+																		stage1 = true;
+
+
+																}
+														}
+												}
+										}
+
+								} else {
+										if (startPos.y < 315f && startPos.y > 260f) {
+												if (touch.position.y < 150f && touch.position.y > 65f) {
+														if (startPos.x > 50f && startPos.x < 110f) {
+																if (touch.position.x > 150f && touch.position.x < 210f) {
+																		stage2 = true;
+
+																}
+														}
+												}
+										}
+								}
+								break;
+						}
+				}
 		}
 	}
 }
